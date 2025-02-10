bc4defedbce19a3513cbcf8e863b8df1e179e7d8
vamsipoluru2 <142745492+vamsipoluru2@users.noreply.github.com>
2025-02-10 12:26:23 +0530

    thid my first commit

---
+++ 02-Functions/VarArgs.java
@@ -0,0 +1,51 @@
+
+<<<<<<< HEAD
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(2,3,4,5,6,7,8);
+        multiple(2, 3, "Kunal", "Rahul", "dvytsbhusc");
+        demo("Kunal", "Rahul", "dvytsbhusc");
+    }
+
+
+    static void demo(String ...v) {// can use more than 0 arumnets
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String ...v) {
+
+    }
+
+    static void fun(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
+=======
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(2,3,4,5,6,7,8);
+        multiple(2, 3, "Kunal", "Rahul", "dvytsbhusc");
+        demo("Kunal", "Rahul", "dvytsbhusc");
+    }
+
+
+    static void demo(String ...v) {// can use more than 0 arumnets
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String ...v) {
+
+    }
+
+    static void fun(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
+
+>>>>>>> aea7af24e81d5c960f837402c42f33c307a76b56
