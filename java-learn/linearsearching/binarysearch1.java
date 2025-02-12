1fa06af3477bc84424d7472ebad104a419bdeb0a
vamsipoluru2 <142745492+vamsipoluru2@users.noreply.github.com>
2025-02-12 07:45:05 +0530

    Add files via upload

---
+++ java-learn/linearsearching/binarysearch1.java
@@ -0,0 +1,25 @@
+public class binarysearch1 {
+    public static void main(String[] args) {
+        int[] arr={2,4,6,9,11,12,14,20,36,48};
+        int target=20;
+        System.out.println(binarySearch(arr, target));
+    }   
+
+    static int binarySearch(int[] arr, int target){ 
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            // int mid=(start+end)/2;//start+end) if large value it may exceed the range of int
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }else if(target>arr[mid]){
+                start=mid+1;
+            }else{
+            return mid;
+            }
+        }
+        return -1;//element not found
+    }
+}
+
